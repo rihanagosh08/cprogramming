602e71986d9b5131f87ddf2058f8efe26fb40408
rihanagosh08 <47448508+rihanagosh08@users.noreply.github.com>
2019-02-08 17:50:11 +0530

    Rename negative.c to neg.c

---
+++ neg.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n<0)
+    {
+    printf("negative");
+    }
+  else if(n>0)
+  {
+      printf("positive");
+     
+  }
+  else
+  {
+      printf("zero");
+  }
+    return 0;
+}
